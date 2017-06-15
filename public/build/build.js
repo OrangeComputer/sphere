dbdc1eace4b0c95c74fd2c89799ad6c0ae1fd061
Adam Harpur <adam@adamharpur.com>
2017-06-15 07:49:32 -0500

    cleaned up package.json

--- public/build/build.js
+++ public/build/build.js
@@ -15080,7 +15080,6 @@
 	// 		<User-List :list.sync="users"></User-List>
 	//
 	// 	</div>
-	// 	<pre>{{users| json}}</pre>
 	// </template>
 	//
 	// <script>
@@ -25117,7 +25116,7 @@
 /* 23 */
 /***/ (function(module, exports) {
 
-	module.exports = "\n<div class='col-sm-6 col-sm-offset-3'>\n\t<h1>Customers</h1>\n\n\t<spinner v-ref:spinner size=\"xl\" fixed text=\"Loading Customers\"></spinner>\n\t<typeahead :data=\"userNames\" :on-hit=\"goToUserPage\" track-by=\"$index\" placeholder=\"Search Customers\">\n\t\t</typeahead>\n\n\t<User-List :list.sync=\"users\"></User-List>\n\n</div>\n<pre>{{users| json}}</pre>\n";
+	module.exports = "\n<div class='col-sm-6 col-sm-offset-3'>\n\t<h1>Customers</h1>\n\n\t<spinner v-ref:spinner size=\"xl\" fixed text=\"Loading Customers\"></spinner>\n\t<typeahead :data=\"userNames\" :on-hit=\"goToUserPage\" track-by=\"$index\" placeholder=\"Search Customers\">\n\t\t</typeahead>\n\n\t<User-List :list.sync=\"users\"></User-List>\n\n</div>\n";
 
 /***/ }),
 /* 24 */
