76dbeba504c4719f4cebc64de8dd804c0c956bd8
adam <adam@adamharpur.com>
2018-04-03 07:55:58 -0500

    feat(Users.vue):filtering on autocomplete

--- public/src/components/Users.vue
+++ public/src/components/Users.vue
@@ -1,36 +1,30 @@
 <template>
-    <section>
-        <p class="content"><b>Selected:</b> {{ selected }}</p>
-        <b-field label="Search Customers">
-            <b-autocomplete
-                v-model="name"
-                :data="data"
-                placeholder="Search Customers"
-                field="first_name"
-                :loading="isFetching"
-                @input="getAsyncData"
-                @select="option => selected = option">
+  <section>
+    <p class="content">
+      <b>Selected:</b> {{ selected }}</p>
+    <b-field label="Search Customers">
+      <b-autocomplete v-model="name" :keep-first=true :data="filteredDataObj" placeholder="Search Customers" field="first_name" :loading="isFetching"
+        @select="option => selected = option">
 
-                <template slot-scope="props">
-                    <div class="media">
-                        <div class="media-content">
-                            {{ props.option.first_name }}
-                             {{ props.option.last_name }}
-                            <br>
-                            <small>
-                                <p v-if="props.option.Emails.length">
-                                  Email:
-                                  <b>{{ props.option.Emails[0].email}}</b>
-                                </p>
-                                Customer ID:
-                                <b>{{ props.option.id }}</b>
-                            </small>
-                        </div>
-                    </div>
-                </template>
-            </b-autocomplete>
-        </b-field>
-    </section>
+        <template class="select" slot-scope="props">
+          <div class="media">
+            <div class="media-content">
+              {{ props.option.first_name }} {{ props.option.last_name }}
+              <br>
+              <small>
+                <p v-if="props.option.Emails.length">
+                  Email:
+                  <b>{{ props.option.Emails[0].email}}</b>
+                </p>
+                Customer ID:
+                <b>{{ props.option.id }}</b>
+              </small>
+            </div>
+          </div>
+        </template>
+      </b-autocomplete>
+    </b-field>
+  </section>
 </template>
 
 <script>
@@ -45,19 +39,34 @@ export default {
       isFetching: false
     };
   },
+  created() {
+    this.getUserData();
+  },
+  computed: {
+    filteredDataObj() {
+      return this.data.filter(option => {
+        let val = "";
+        if (option.first_name) {
+          let fullName = `${option.first_name}${option.last_name}`;
+          return (
+            fullName
+              .toString()
+              .toLowerCase()
+              .indexOf(this.name.toLowerCase().replace(/ /g, "")) >= 0
+          );
+        }
+      });
+    }
+  },
   methods: {
-    // You have to install and import debounce to use it,
-    // it's not mandatory though.
-    getAsyncData: debounce(function() {
+    getUserData: debounce(function() {
       this.data = [];
       this.isFetching = true;
       this.axios
         .get(`/api/users`)
         .then(({ data }) => {
           data.forEach((item, pos) => {
-            if (pos < 20) {
-              this.data.push(item);
-            }
+            this.data.push(item);
           });
           this.isFetching = false;
         })
