e365284eff1b4761ebf59b109e6dbdd78440e026
adam <adam@adamharpur.com>
2018-04-07 17:12:22 -0500

    feat(index.js):dynamic routing from .json

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,22 +1,6 @@
 Item = require("../models/").Item;
-utils = require("./utils");
 module.exports = {
   index(req, res) {
-    Item.findAll({
-      // return the tables associated with each user
-      //   include: [
-      //     {
-      //       model: Item
-      //     }
-      //   ]
-    })
-      .then(function(products) {
-        console.log(products);
-        utils.sendResult(res, products);
-      })
-      .catch(function(error) {
-        console.log(error);
-        utils.sendError(res, error);
-      });
+    return Item.findAll({});
   }
 };
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,11 +3,10 @@ Receipt = require("../models").Receipt;
 Email = require("../models").Email;
 Phone = require("../models").Phone;
 Address = require("../models").Address;
-utils = require("./utils");
 
 module.exports = {
   index(req, res) {
-    User.findAll({
+    return User.findAll({
       // return the tables associated with each user
       include: [
         {
@@ -27,58 +26,32 @@ module.exports = {
           attributes: ["street", "city", "state", "zip_code"]
         }
       ]
-    })
-      .then(function(users) {
-        utils.sendResult(res, users);
-      })
-      .catch(function(error) {
-        utils.sendError(res, error);
-      });
+    });
   },
   show(req, res) {
-    User.findById(req.params.id, {
+    return User.findById(req.params.id, {
       // return all receipts for user
       include: Receipt
-    })
-      .then(function(user) {
-        utils.sendResult(res, user);
-      })
-      .catch(function(error) {
-        utils.sendError(res, error);
-      });
+    });
   },
   create(req, res) {
-    User.create({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name
-    })
-      .then(function(newUser) {
-        utils.sendResult(res, newUser);
-      })
-      .catch(function(error) {
-        utils.sendError(res, error);
-      });
+    //  return User.create({
+    //    first_name: req.body.first_name,
+    //    last_name: req.body.last_name
+    //  });
   },
   update(req, res) {
-    User.find({
+    return User.find({
       where: {
         id: req.params.id
       }
     }).then(function(user) {
       if (user) {
-        console.log(req.body);
-        user
-          .updateAttributes({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            middle_name: req.body.middle_name
-          })
-          .then(function(updatedUser) {
-            utils.sendResult(res, updatedUser);
-          })
-          .catch(function(err) {
-            utils.sendError(res, err);
-          });
+        user.updateAttributes({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          middle_name: req.body.middle_name
+        });
       }
     });
   },
